f588488421a428c4e27abc1bfc70f808a35e1627
Yadu Krishnan <yadavan88@gmail.com>
2024-05-14 18:29:48 +0200

    Modified renamer

--- internal-scripts/ClassRenamer.scala
+++ internal-scripts/ClassRenamer.scala
@@ -1,6 +1,6 @@
-/**
-  * This is an internal scala-cli script written just to reduce manual work of renaming class names.
-  * This doesn't fully correct all the class names, however reduces the manual effort by 90%.
+/** This is an internal scala-cli script written just to reduce manual work of
+  * renaming class names. This doesn't fully correct all the class names,
+  * however reduces the manual effort by 90%.
   */
 //> using toolkit default
 import os._
@@ -9,23 +9,41 @@ object RenameClassNames {
   def main(args: Array[String]): Unit = {
     val directory = os.pwd / os.up
 
-    os.walk(directory).filter(_.ext == "scala").filter(_.toString.contains("src/test/scala")).foreach { filePath =>
-      val fileName = filePath.last
-      val content = os.read(filePath)
+    os.walk(directory)
+      .filter(_.ext == "scala")
+      .filter(_.toString.contains("src/test/scala"))
+      .filter(f =>
+        f.toString.endsWith("Test.scala") || f.toString.endsWith("Spec.scala")
+      )
+      .foreach { filePath =>
+        val fileName = filePath.last
+        val content = os.read(filePath)
 
-      val existingClassName = getClassFromContent(content)
+        val existingClassName = getClassFromContent(content)
 
-      val fileNameWithoutExtension = fileName.dropRight(6)
+        val fileNameWithoutExtension = fileName.dropRight(6)
 
-      // Rename the class if it doesn't match the filename
-      if (!existingClassName.toLowerCase.startsWith("case ") && existingClassName.toLowerCase != fileNameWithoutExtension.toLowerCase) {
-        // Update the content with the new class name
-        val updatedContent = content.replaceAll(s"class\\s+$existingClassName", s"class $fileNameWithoutExtension")
+        // Rename the class if it doesn't match the filename
+        if (
+          !existingClassName.toLowerCase.startsWith(
+            "case "
+          ) && existingClassName.toLowerCase != fileNameWithoutExtension.toLowerCase
+        ) {
+          // Update the content with the new class name
+          val updatedContent = content.replaceAll(
+            s"class\\s+$existingClassName",
+            s"class $fileNameWithoutExtension"
+          )
 
-        os.write.over(filePath, updatedContent)
-        println(s"Renamed class in $fileName to $fileNameWithoutExtension")
+          val newFileName = filePath.toString.replace("Test.scala", "UnitTest.scala").replace("Spec.scala", "UnitTest.scala")
+          //rename file
+          os.move(filePath, os.Path(newFileName))
+
+          //Now, rename the class
+          os.write.over(filePath, newFileName)
+          println(s"Renamed class in $fileName to $fileNameWithoutExtension")
+        }
       }
-    }
   }
 
   // Extract class name from file content
@@ -34,7 +52,7 @@ object RenameClassNames {
     val matchResult = classNameRegex.findFirstMatchIn(content)
     matchResult match {
       case Some(m) => m.group(1)
-      case None => ""
+      case None    => ""
     }
   }
 }
